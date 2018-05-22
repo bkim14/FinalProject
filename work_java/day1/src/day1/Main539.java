28b604cdbf61d9cd61139e6d5ad87a6cd171b02d
bkim14 <qnddhr12@gmail.com>
2018-05-22 16:12:14 +0900

    하이 친구들

--- work_java/day1/src/day1/Main539.java
+++ work_java/day1/src/day1/Main539.java
@@ -5,23 +5,7 @@ public class Main539 {
 
 	public static void main(String[] args) {
 	
-		Scanner scan=new Scanner(System.in);
-		int x=1;
-		int sum=0;
-		int count=0;
-		double mean=0;
-
-		while(x<100) {
-			x=scan.nextInt();
-			count++;
-			sum=x+sum;
-			mean=(double) sum/count;
-
-		}
-		
-		System.out.println(sum);
-		
-		System.out.printf("%.1f", mean);
+		System.out.println("하이 친구들");
 		
 	}
 
